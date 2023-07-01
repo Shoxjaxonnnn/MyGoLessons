aab5a0d9d5175262f811aacbd023bb1d29b4b381
Shoxjaxonnnn <shohjaxonshukurullayev7@gmail.com>
2023-07-01 12:52:14 +0500

    Conditionals on golang

---
+++ 2.Conditionals/step1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main () {
+
+  a := 5
+  d := 8
+
+    if a == d {
+	  fmt.Println("Ikkalasi teng")
+    }else if a > d {
+	  fmt.Println("a katta d dan ")
+	}else if d > a  {
+	  fmt.Println("a kichkina d dan ")
+	}else {
+		fmt.Println("kalla qoymading") 
+	}
+}
